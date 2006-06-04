48fb401b159e6a19a0c505e1e08ab167f6a15458
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2006-06-04 19:17:58 +0000

    Help files moved from CVS

---
+++ Trunk/Help/Update.bat
@@ -0,0 +1,7 @@
+@echo off
+set hcw="D:\Program Files\Borland\CBuilder6\Help\Tools\hcw.exe"
+%hcw% /C /E English\Graph.hpj
+%hcw% /C /E Danish\Graph.hpj
+%hcw% /C /E German\Graph.hpj
+%hcw% /C /E French\Graph.hpj
+%hcw% /C /E Slovenian\Graph.hpj
