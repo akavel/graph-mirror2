d9580206a8bf8615ed7154c4900dd2bc9eec1a65
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2006-08-31 08:41:31 +0000

    Media Player component finished I think.

--- Components/TMediaPlayerEx/MediaPlayerEx.cpp
+++ Components/TMediaPlayerEx/MediaPlayerEx.cpp
@@ -24,21 +24,22 @@ namespace Mediaplayerex
 }
 //---------------------------------------------------------------------------
 __fastcall TMediaPlayerEx::TMediaPlayerEx(TComponent* Owner)
-  : TMediaPlayer(Owner), FOnSignal(NULL), FRepeat(false), FReverse(false), FOnHint(NULL),
-    LastButton(static_cast<TMPBtnType>(0xFF))
+  : TComponent(Owner), FOnSignal(NULL), FRepeat(false), FReverse(false), FDisplay(NULL), FOnNotify(NULL),
+    FNotify(false), DeviceID(0), Handle(0), OldWindowProc(NULL), FShareable(false), FDeviceType(dtAutoSelect),
+    FFrom(-1), FTo(-1), FSpeed(1000)
 {
 }
 //---------------------------------------------------------------------------
-void __fastcall TMediaPlayerEx::MMSignal(TMessage &Message)
+__fastcall TMediaPlayerEx::~TMediaPlayerEx()
 {
-  if(OnSignal)
-    OnSignal(this, Message.LParam);
+  Close();
 }
 //---------------------------------------------------------------------------
 void TMediaPlayerEx::SetSignal(unsigned At, unsigned Interval)
 {
+  CheckIfOpen();
   MCI_DGV_SIGNAL_PARMS SignalParm;
-  SignalParm.dwCallback = reinterpret_cast<DWORD>(Handle);
+  SignalParm.dwCallback = Handle;
   SignalParm.dwPosition = At;
   SignalParm.dwPeriod = Interval;
   SignalParm.dwUserParm = 0;
@@ -51,8 +52,9 @@ void TMediaPlayerEx::SetSignal(unsigned At, unsigned Interval)
 //---------------------------------------------------------------------------
 void TMediaPlayerEx::RemoveSignal(unsigned At)
 {
+  CheckIfOpen();
   MCI_DGV_SIGNAL_PARMS SignalParm;
-  SignalParm.dwCallback = reinterpret_cast<DWORD>(Handle);
+  SignalParm.dwCallback = Handle;
   SignalParm.dwUserParm = At;
 
   mciSendCommand(DeviceID, MCI_SIGNAL, MCI_WAIT | MCI_DGV_SIGNAL_CANCEL | MCI_DGV_SIGNAL_USERVAL, reinterpret_cast<DWORD>(&SignalParm));
@@ -60,23 +62,201 @@ void TMediaPlayerEx::RemoveSignal(unsigned At)
 //---------------------------------------------------------------------------
 void TMediaPlayerEx::RemoveAllSignals()
 {
+  CheckIfOpen();
   MCI_DGV_SIGNAL_PARMS SignalParm;
-  SignalParm.dwCallback = reinterpret_cast<DWORD>(Handle);
+  SignalParm.dwCallback = Handle;
 
   mciSendCommand(DeviceID, MCI_SIGNAL, MCI_WAIT | MCI_DGV_SIGNAL_CANCEL, reinterpret_cast<DWORD>(&SignalParm));
 }
 //---------------------------------------------------------------------------
-void TMediaPlayerEx::Play()
+void __fastcall TMediaPlayerEx::WndProc(Messages::TMessage &Message)
 {
-  if(!Repeat && !Reverse)
+  switch(Message.Msg)
   {
-    TMediaPlayer::Play();
-    return;
+    case MM_MCISIGNAL:
+      if(OnSignal)
+        OnSignal(this, Message.LParam);
+      break;
+
+    case MM_MCINOTIFY:
+    {
+      TMPNotifyValues NotifyValue = nvFailure;
+      switch(Message.WParam)
+      {
+        case MCI_NOTIFY_SUCCESSFUL: NotifyValue = nvSuccessful; break;
+        case MCI_NOTIFY_SUPERSEDED: NotifyValue = nvSuperseded; break;
+        case MCI_NOTIFY_ABORTED:    NotifyValue = nvAborted; break;
+      }
+      if(OnNotify)
+        OnNotify(this, NotifyValue);
+      break;
+    }
+    case WM_PAINT:
+      if(DeviceID == 0 || Display == NULL)
+      {
+        if(OldWindowProc)
+          OldWindowProc(Message);
+      }
+      else
+      {
+        PAINTSTRUCT Paint;
+        HDC DC = BeginPaint(Display->Handle, &Paint);
+        MCI_DGV_UPDATE_PARMS UpdateParms = {0};
+        UpdateParms.hDC = DC; //reinterpret_cast<HDC>(Message.WParam);
+        DWORD Flags = MCI_WAIT | MCI_DGV_UPDATE_HDC | MCI_DGV_UPDATE_PAINT;
+        mciSendCommand(DeviceID, MCI_UPDATE, Flags, reinterpret_cast<DWORD>(&UpdateParms));
+        EndPaint(Display->Handle, &Paint);
+        Message.Result = 0;
+      }
+      break;
+
+    default:
+      if(OldWindowProc)
+        OldWindowProc(Message);
+      break;
+  }
+}
+//---------------------------------------------------------------------------
+void __fastcall TMediaPlayerEx::SetDisplay(TWinControl *Control)
+{
+  if(FDisplay)
+    FDisplay->WindowProc = OldWindowProc;
+
+  FDisplay = Control;
+  Handle = 0;
+  OldWindowProc = NULL;
+
+  if(Control)
+  {
+    OldWindowProc = Control->WindowProc;
+    Control->WindowProc = &WndProc;
+    Handle = reinterpret_cast<DWORD>(Control->Handle);
+    Control->FreeNotification(this);
   }
 
-  TMCI_Play_Parms PlayParm;
+  if(DeviceID != 0 && FCapabilities.Contains(mpUsesWindow))
+  {
+    MCI_DGV_WINDOW_PARMS AWindowParm;
+    DWORD Flags = MCI_WAIT | MCI_DGV_WINDOW_HWND;
+    AWindowParm.hWnd = (Control != NULL) ? Control->Handle : reinterpret_cast<HANDLE>(MCI_DGV_WINDOW_DEFAULT);
+    MciCheck(mciSendCommand(DeviceID, MCI_WINDOW, Flags, reinterpret_cast<DWORD>(&AWindowParm)));
+  }
+}
+//---------------------------------------------------------------------------
+void __fastcall TMediaPlayerEx::SetTimeFormat(TMPTimeFormats Format)
+{
+  CheckIfOpen();
+  MCI_SET_PARMS SetParm;
+  DWORD Flags = MCI_WAIT | MCI_SET_TIME_FORMAT;
+  SetParm.dwTimeFormat = Format;
+  MciCheck(mciSendCommand(DeviceID, MCI_SET, Flags, reinterpret_cast<DWORD>(&SetParm)));
+}
+//---------------------------------------------------------------------------
+DWORD TMediaPlayerEx::GetStatus(DWORD Item)
+{
+  MCI_STATUS_PARMS StatusParm;
+  CheckIfOpen();
+  DWORD Flags = MCI_WAIT | MCI_STATUS_ITEM;
+  StatusParm.dwItem = Item;
+  MciCheck(mciSendCommand(DeviceID, MCI_STATUS, Flags, reinterpret_cast<DWORD>(&StatusParm)));
+  return StatusParm.dwReturn;
+}
+//---------------------------------------------------------------------------
+TMPTimeFormats __fastcall TMediaPlayerEx::GetTimeFormat()
+{
+  return static_cast<TMPTimeFormats>(GetStatus(MCI_STATUS_TIME_FORMAT));
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Seek(unsigned Value, bool Wait, bool Notify)
+{
+  MCI_SEEK_PARMS SeekParm;
+  CheckIfOpen();
 
-  //raise exception if device is not open
+  DWORD Flags = Value == 0 ? MCI_SEEK_TO_START : MCI_TO;
+
+  if(Wait)
+    Flags |= MCI_WAIT;
+  if(Notify)
+    Flags |= MCI_NOTIFY;
+
+  SeekParm.dwCallback = Handle;
+  SeekParm.dwTo = Value;
+  MciCheck(mciSendCommand(DeviceID, MCI_SEEK, Flags, reinterpret_cast<DWORD>(&SeekParm)));
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::SetPosition(int Value)
+{
+  if(Value != GetPosition())
+    Seek(Value, true, false);
+}
+//---------------------------------------------------------------------------
+int TMediaPlayerEx::GetPosition()
+{
+  return GetStatus(MCI_STATUS_POSITION);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::GetDeviceCaps()
+{
+  MCI_GETDEVCAPS_PARMS DevCapParm;
+
+  WORD Flags = MCI_WAIT | MCI_GETDEVCAPS_ITEM;
+
+  DevCapParm.dwItem = MCI_GETDEVCAPS_CAN_PLAY;
+  MciCheck(mciSendCommand(DeviceID, MCI_GETDEVCAPS, Flags, reinterpret_cast<DWORD>(&DevCapParm)));
+  if(DevCapParm.dwReturn)
+    FCapabilities << mpCanPlay;
+
+  DevCapParm.dwItem = MCI_GETDEVCAPS_CAN_RECORD;
+  MciCheck(mciSendCommand(DeviceID, MCI_GETDEVCAPS, Flags, reinterpret_cast<DWORD>(&DevCapParm)));
+  if(DevCapParm.dwReturn)
+    FCapabilities << mpCanRecord;
+
+  DevCapParm.dwItem = MCI_GETDEVCAPS_CAN_EJECT;
+  MciCheck(mciSendCommand(DeviceID, MCI_GETDEVCAPS, Flags, reinterpret_cast<DWORD>(&DevCapParm)));
+  if(DevCapParm.dwReturn)
+    FCapabilities << mpCanEject;
+
+  DevCapParm.dwItem = MCI_GETDEVCAPS_HAS_VIDEO;
+  MciCheck(mciSendCommand(DeviceID, MCI_GETDEVCAPS, Flags, reinterpret_cast<DWORD>(&DevCapParm)));
+  if(DevCapParm.dwReturn)
+    FCapabilities << mpUsesWindow;
+
+  DevCapParm.dwItem = MCI_GETDEVCAPS_DEVICE_TYPE;
+  MciCheck(mciSendCommand(DeviceID, MCI_GETDEVCAPS, Flags, reinterpret_cast<DWORD>(&DevCapParm)));
+  if(DevCapParm.dwReturn == MCI_DEVTYPE_ANIMATION ||
+     DevCapParm.dwReturn == MCI_DEVTYPE_DIGITAL_VIDEO ||
+     DevCapParm.dwReturn == MCI_DEVTYPE_OVERLAY ||
+     DevCapParm.dwReturn == MCI_DEVTYPE_VCR)
+       FCapabilities << mpCanStep;
+
+  MCI_ANIM_RECT_PARMS RectParms;
+  MciCheck(mciSendCommand(DeviceID, MCI_WHERE, MCI_ANIM_WHERE_SOURCE, reinterpret_cast<DWORD>(&RectParms)));
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::MciCheck(MCIERROR Error)
+{
+  if(Error != 0)
+  {
+    char Str[128];
+    mciGetErrorString(Error, Str, sizeof(Str));
+    throw EMCIDeviceError(Str);
+  }
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::CheckIfOpen()
+{
+  if(DeviceID == 0)
+    throw EMCIDeviceError("Device not open");
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Play(bool Wait)
+{
+  CheckIfOpen();
+  int End = (To == -1) ? Length-1 : To;
+  if(!Reverse && AutoRewind && Position >= End)
+    Seek(0, true, false);
+
+  MCI_PLAY_PARMS PlayParm;
 
   DWORD Flags = 0;
 
@@ -88,47 +268,197 @@ void TMediaPlayerEx::Play()
     Flags |= MCI_DGV_PLAY_REPEAT;
   if(Reverse)
     Flags |= MCI_DGV_PLAY_REVERSE;
+  if(FFrom != -1)
+    Flags |= MCI_FROM;
+  if(FTo != -1)
+    Flags |= MCI_TO;
 
-  PlayParm.dwCallback = reinterpret_cast<DWORD>(Handle);
-  mciSendCommand(DeviceID, MCI_PLAY, Flags, reinterpret_cast<DWORD>(&PlayParm));
+  PlayParm.dwCallback = Handle;
+  PlayParm.dwFrom = FFrom;
+  PlayParm.dwTo = FTo;
+  MciCheck(mciSendCommand(DeviceID, MCI_PLAY, Flags, reinterpret_cast<DWORD>(&PlayParm)));
 }
 //---------------------------------------------------------------------------
-void __fastcall TMediaPlayerEx::Click(TMPBtnType Button, bool &DoDefault)
+void TMediaPlayerEx::StepFrames(unsigned Frames, bool Reverse, bool Wait)
 {
-  TMediaPlayer::Click(Button, DoDefault);
-  if(DoDefault && Button == btPlay)
+  CheckIfOpen();
+
+  if(Capabilities.Contains(mpUsesWindow))
   {
-    Play();
-    DoDefault = false;
+    MCI_DGV_STEP_PARMS StepParm;
+    DWORD Flags = MCI_DGV_STEP_FRAMES;
+    if(Wait)
+      Flags |= MCI_WAIT;
+    if(Notify)
+      Flags |= MCI_NOTIFY;
+    if(Reverse)
+      Flags |= MCI_DGV_STEP_REVERSE;
+    StepParm.dwFrames = Frames;
+    StepParm.dwCallback = Handle;
+    MciCheck(mciSendCommand(DeviceID, MCI_STEP, Flags, reinterpret_cast<DWORD>(&StepParm)));
   }
 }
 //---------------------------------------------------------------------------
-TMPBtnType TMediaPlayerEx::FindButton(unsigned X)
+void TMediaPlayerEx::Close(bool Wait)
 {
-  unsigned VisibleButtonCount = 0;
-  for(TMPBtnType Button = btPlay; Button <= btEject; Button = static_cast<TMPBtnType>(Button+1))
-    if(VisibleButtons.Contains(Button))
-      VisibleButtonCount++;
+  if(DeviceID == 0)
+    return;
+  MciGeneric(MCI_CLOSE, 0, Wait, Notify);
+  DeviceID = 0;
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::MciGeneric(DWORD Command, DWORD Flags, bool Wait, bool Notify)
+{
+  CheckIfOpen();
+  MCI_GENERIC_PARMS GenParm;
 
-  unsigned ButtonWidth = (Width - 1) / VisibleButtonCount + 1;
-  unsigned VisibleBtn = X / ButtonWidth;
-  for(TMPBtnType Button = btPlay; Button <= btEject; Button = static_cast<TMPBtnType>(Button+1))
-    if(VisibleButtons.Contains(Button))
-      if(VisibleBtn-- == 0)
-        return Button;
-  return static_cast<TMPBtnType>(0xFF);      
+  if(Wait)
+    Flags |= MCI_WAIT;
+  if(Notify)
+    Flags |= MCI_NOTIFY;
+
+  GenParm.dwCallback = Handle;
+  MciCheck(mciSendCommand(DeviceID, Command, Flags, reinterpret_cast<DWORD>(&GenParm)));
 }
 //---------------------------------------------------------------------------
-void __fastcall TMediaPlayerEx::WMMouseMove(TWMMouseMove &Message)
+void TMediaPlayerEx::EjectDoor(bool Wait)
 {
-  if(OnHint)
-  {
-    TMPBtnType NewButton = FindButton(Message.XPos);
-    if(NewButton != LastButton)
-      OnHint(this, NewButton);
-    LastButton = NewButton;
-  }
-  TMediaPlayer::Dispatch(&Message);
+  MciGeneric(MCI_SET, MCI_SET_DOOR_OPEN, Wait, Notify);
 }
 //---------------------------------------------------------------------------
+void TMediaPlayerEx::CloseDoor(bool Wait)
+{
+  MciGeneric(MCI_SET, MCI_SET_DOOR_CLOSED, Wait, Notify);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Next(bool Wait)
+{
+  CheckIfOpen();
+
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Open(bool Wait)
+{
+const char* DeviceName[] = {"", "AVIVideo", "CDAudio", "DAT",
+    "DigitalVideo", "MMMovie", "Other", "Overlay", "Scanner", "Sequencer",
+    "VCR", "Videodisc" "WaveAudio"};
+
+  MCI_OPEN_PARMS OpenParm;
+  TRect DisplayR;
+
+  if(DeviceID != 0)
+    Close(); //Must close MCI Device first before opening another}
+
+  OpenParm.dwCallback = Handle;
+  OpenParm.lpstrDeviceType = DeviceName[FDeviceType];
+  OpenParm.lpstrElementName = FFileName.c_str();
+
+  DWORD Flags = 0;
+
+  if(Wait)
+    Flags |= MCI_WAIT;
+
+  if(Notify)
+    Flags |= MCI_NOTIFY;
+
+  if(FDeviceType != dtAutoSelect)
+    Flags |= MCI_OPEN_TYPE;
+  else
+    Flags |= MCI_OPEN_ELEMENT;
+
+  if(FShareable)
+    Flags |= MCI_OPEN_SHAREABLE;
+
+  MciCheck(mciSendCommand(0, MCI_OPEN, Flags, reinterpret_cast<DWORD>(&OpenParm)));
+
+  DeviceID = OpenParm.wDeviceID;
+  FFrames = Length / 10;  //Default frames to step = 10% of total frames
+  GetDeviceCaps(); //Must first get device capabilities}
+  if(FCapabilities.Contains(mpUsesWindow)) //Used for video output positioning
+    SetDisplay(FDisplay); //If one was set in design mode
+
+  if(FSpeed != 1000)
+    SetSpeed(FSpeed);
+
+  if(FDeviceType == dtCDAudio || FDeviceType == dtVideodisc)
+    TimeFormat = tfTMSF; //Set timeformat to use tracks
+}
+//---------------------------------------------------------------------------
+void __fastcall TMediaPlayerEx::Notification(TComponent* AComponent, TOperation Operation)
+{
+  if(Operation == opRemove && AComponent == FDisplay)
+    SetDisplay(NULL);
+  TComponent::Notification(AComponent, Operation);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Pause(bool Wait)
+{
+  MciGeneric(MCI_PAUSE, 0, Wait, Notify);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Resume(bool Wait)
+{
+  MciGeneric(MCI_RESUME, 0, Wait, Notify);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Previous(bool Wait)
+{
+  CheckIfOpen();
+
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Step(bool Wait)
+{
+  int End = (To == -1) ? Length-1 : To;
+  if(FAutoRewind && Position >= End)
+    Seek(0, Wait, true);
+  else
+    StepFrames(FFrames, false, Wait);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Back(bool Wait)
+{
+  int StepValue = Frames;
+  if(Position == Length)
+    StepValue++;
+  StepFrames(StepValue, true, Wait);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Stop(bool Wait)
+{
+  MciGeneric(MCI_STOP, 0, Wait, Notify);
+}
+//---------------------------------------------------------------------------
+void TMediaPlayerEx::Rewind(bool Wait)
+{
+  CheckIfOpen();
+  Seek(0, Wait, Notify);
+}
+//---------------------------------------------------------------------------
+int TMediaPlayerEx::GetLength()
+{
+  return GetStatus(MCI_STATUS_LENGTH);
+}
+//---------------------------------------------------------------------------
+TMPModes TMediaPlayerEx::GetMode()
+{
+  return static_cast<TMPModes>(GetStatus(MCI_STATUS_MODE) - 524); //MCI Mode #s are 524+enum
+}
+//---------------------------------------------------------------------------
+void __fastcall TMediaPlayerEx::SetSpeed(int Value)
+{
+  if(Value <= 0)
+    return;
+
+  FSpeed = Value;
+  if(DeviceID != 0)
+  {
+    MCI_DGV_SET_PARMS SetParm;
+    DWORD Flags = MCI_WAIT | MCI_DGV_SET_SPEED;
+    SetParm.dwSpeed = Value;
+    MciCheck(mciSendCommand(DeviceID, MCI_SET, Flags, reinterpret_cast<DWORD>(&SetParm)));
+  }
+}
+//---------------------------------------------------------------------------
+
 
--- Components/TMediaPlayerEx/MediaPlayerEx.h
+++ Components/TMediaPlayerEx/MediaPlayerEx.h
@@ -9,41 +9,95 @@
 //---------------------------------------------------------------------------
 class TMediaPlayerEx;
 typedef void __fastcall (__closure *TSignalEvent)(TMediaPlayerEx* Sender, unsigned Position);
-typedef void __fastcall (__closure *TMPHintEvent)(TMediaPlayerEx* Sender, TMPBtnType Button);
+typedef void __fastcall (__closure *TNotifyEvent)(TMediaPlayerEx* Sender, TMPNotifyValues NotifyValue);
 
-class PACKAGE TMediaPlayerEx : public TMediaPlayer
+class PACKAGE TMediaPlayerEx : public TComponent
 {
 private:
   TSignalEvent FOnSignal;
-  TMPHintEvent FOnHint;
-  TMPBtnType LastButton;
+  ::TNotifyEvent FOnNotify;
   bool FRepeat;
   bool FReverse;
+  bool FNotify;
+  bool FShareable;
+  bool FAutoRewind;
+  int FFrom;
+  int FTo;
+  int FFrames;
+  int FSpeed;
+  TWinControl *FDisplay;
+  AnsiString FFileName;
+  TMPDevCapsSet FCapabilities;
+  TMPDeviceTypes FDeviceType;
 
-  TMPBtnType FindButton(unsigned X);
+  unsigned DeviceID;
+  DWORD Handle;
+  TWndMethod OldWindowProc;
+  TRect WorkR;
 
-  void __fastcall MMSignal(TMessage &Message);
-  void __fastcall WMMouseMove(TWMMouseMove &Message);
-  DYNAMIC void __fastcall Click(TMPBtnType Button, bool &DoDefault);
+  void __fastcall WndProc(Messages::TMessage &Message);
+  void GetDeviceCaps();
+  void MciCheck(MCIERROR Error);
+  void CheckIfOpen();
+  void StepFrames(unsigned Frames, bool Reverse, bool Wait);
+  DWORD GetStatus(DWORD Item);
+  void Seek(unsigned Value, bool Wait, bool Notify);
+  void MciGeneric(DWORD Command, DWORD Flags, bool Wait, bool Notify);
 
-BEGIN_MESSAGE_MAP
-  VCL_MESSAGE_HANDLER(MM_MCISIGNAL, TMessage, MMSignal)
-  VCL_MESSAGE_HANDLER(WM_MOUSEMOVE, TWMMouseMove, WMMouseMove)
-END_MESSAGE_MAP(TMediaPlayer)
+  void __fastcall SetDisplay(TWinControl *Control);
+  void __fastcall SetTimeFormat(TMPTimeFormats Format);
+  TMPTimeFormats __fastcall GetTimeFormat();
+  void SetPosition(int Value);
+  int GetPosition();
+  int GetLength();
+  TMPModes GetMode();
+  void __fastcall SetSpeed(int Value);
+
+  void __fastcall Notification(TComponent* AComponent, TOperation Operation);
 
 protected:
 public:
   __fastcall TMediaPlayerEx(TComponent* Owner);
+  __fastcall ~TMediaPlayerEx();
   void SetSignal(unsigned At, unsigned Interval = 0);
   void RemoveSignal(unsigned At);
   void RemoveAllSignals();
-  void Play();
+  void Play(bool Wait=false);
+  void Back(bool Wait=true);
+  void Close(bool Wait=true);
+  void EjectDoor(bool Wait=true);
+  void CloseDoor(bool Wait=true);
+  void Next(bool Wait=true);
+  void Open(bool Wait=true);
+  void Pause(bool Wait=true);
+  void Resume(bool Wait=false);
+  void Previous(bool Wait=true);
+  void Step(bool Wait=true);
+  void Stop(bool Wait=true);
+  void Rewind(bool Wait=true);
+
+  __property int Position = {read=GetPosition, write=SetPosition};
+  __property int Frames = {read=FFrames, write=FFrames};
+  __property int Length = {read=GetLength};
+  __property int From = {read=FFrom, write=FFrom, default=-1};
+  __property int To = {read=FTo, write=FTo, default=-1};
+  __property TMPDevCapsSet Capabilities = {read=FCapabilities};
+  __property TMPTimeFormats TimeFormat = {read=GetTimeFormat, write=SetTimeFormat};
+  __property TMPModes Mode = {read=GetMode};
 
 __published:
+  __property TWinControl* Display = {read=FDisplay, write=SetDisplay, default=NULL};
+  __property AnsiString FileName = {read=FFileName, write=FFileName};
+  __property TMPDeviceTypes DeviceType = {read=FDeviceType, write=FDeviceType, default=dtAutoSelect};
   __property bool Repeat = {read=FRepeat, write=FRepeat, default=false};
   __property bool Reverse = {read=FReverse, write=FReverse, default=false};
+  __property bool Notify = {read=FNotify, write=FNotify, default=false};
+  __property bool Shareable = {read=FShareable, write=FShareable, default=false};
+  __property bool AutoRewind = {read=FAutoRewind, write=FAutoRewind, default=false};
+  __property int Speed = {read=FSpeed, write=SetSpeed, default=1000};
+
   __property TSignalEvent OnSignal = {read=FOnSignal, write=FOnSignal, default=NULL};
-  __property TMPHintEvent OnHint = {read=FOnHint, write=FOnHint, default=NULL};
+  __property ::TNotifyEvent OnNotify = {read=FOnNotify, write=FOnNotify, default=NULL};
 };
 //---------------------------------------------------------------------------
 #endif
