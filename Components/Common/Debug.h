f1350cc716c0d6f38cefda0c514d0262080328e9
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2006-10-03 18:50:16 +0000

    More debugging added.

--- Components/Common/Debug.h
+++ Components/Common/Debug.h
@@ -1,17 +1,29 @@
+//---------------------------------------------------------------------------
 #ifndef DebugH
+#define DebugH
 
-void __stdcall OutputDebugStringA(const char* lpOutputString);
-
-class TDebugStreamBuf : public std::stringbuf
+struct TNameValue
 {
-protected:
-  int sync()
-  {
-    OutputDebugStringA(str().c_str());
-    str("");
-    return std::stringbuf::sync();
-  }
+  const char *Name;
+  unsigned Value;
+  TNameValue(const char *AName, unsigned AValue) : Name(AName), Value(AValue) {}
 };
 
+extern const TNameValue OleUIResultList[];
+extern const TNameValue HResultList[];
+
+//Defines and functions used for logging OLE calls
+#define LOG_FUNCTION_CALL(x) DebugLogFunctionCall(#x, HResultList, (x))
+#define LOG_OLEUI_CALL(x) DebugLogFunctionCall(#x, OleUIResultList, (x))
+#define NAME_VALUE_ENTRY(x) TNameValue(#x, x)
+#define NAME_VALUE_END TNameValue(NULL, 0)
+
+HRESULT DebugLogFunctionCall(const char *Name, const TNameValue List[], HRESULT Result);
+AnsiString ValueToStr(const TNameValue List[], unsigned Value);
+AnsiString FlagsToStr(const TNameValue List[], unsigned Value);
+
+void InitDebug();
+
+//---------------------------------------------------------------------------
 #endif
  
