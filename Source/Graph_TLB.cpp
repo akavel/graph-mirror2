6c56003f7d613efba96d6171ab8312d3fe3a3a62
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2006-11-21 20:12:48 +0000

    Some text corrections.

--- Source/Graph_TLB.cpp
+++ Source/Graph_TLB.cpp
@@ -10,7 +10,7 @@
 // ************************************************************************ //
 
 // C++ TLBWRTR : $Revision:   1.151.1.0.1.27  $
-// File generated on 14-11-2006 17:21:40 from Type Library described below.
+// File generated on 18-11-2006 18:41:56 from Type Library described below.
 
 // ************************************************************************  //
 // Type Lib: D:\Projects\Graph\Source\Graph.tlb (1)
