33f9aee88eaaf2a6fd7c5a146ed4e34127ca1347
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2006-12-31 11:13:26 +0000

    Initial plugin implementation.

---
+++ Plugins/Circle.py
@@ -0,0 +1,32 @@
+# File: Circle.py
+import tkSimpleDialog
+import Graph
+from Tkinter import *
+
+class MyDialog(tkSimpleDialog.Dialog):
+    def body(self, master):
+        Label(master, text="Radius:").grid(row=0)
+        Label(master, text="Center:").grid(row=1)
+        Label(master, text="X:").grid(row=1, column=1)
+        Label(master, text="Y:").grid(row=1, column=3)
+
+        self.e1 = Entry(master, width=10)
+        self.e2 = Entry(master, width=10)
+        self.e3 = Entry(master, width=10)
+
+        self.e1.grid(row=0, column=2)
+        self.e2.grid(row=1, column=2)
+        self.e3.grid(row=1, column=4)
+        return self.e1 # initial focus
+
+    def validate(self):
+        #self.result = float(self.e1.get()), float(self.e2.get()), float(self.e3.get())
+        return 1
+
+    def apply(self):
+        Graph.CreateParametricFunction(self.e2.get() + "+" + self.e1.get() + "*cos t", self.e3.get() + "+" + self.e1.get() + "*sin t")
+
+def execute_action():
+	d = MyDialog(None)
+
+Graph.CreateAction("CircleAction", "Create circle...", execute_action, icon="Plugins\\Graph.bmp", hint="Create circle from center and radius", shortcut="Ctrl+Alt+C")
