9b8f1d87259c4b69245e15d35714d96d9cf92e72
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2007-01-02 23:23:31 +0000

    Fixed some problems loading plugins.

--- Plugins/GraphUtil.py
+++ Plugins/GraphUtil.py
@@ -1,10 +1,28 @@
 import os.path
+import os
+import sys
+import imp
+import traceback
 from Tkinter import *
 
 def InitPlugins():
 	print "Loading plugins..."
+	sys.stdin = None
+
+	global root
 	root = Tk()
 	root.withdraw()
-	for FileName in os.listdir("Plugins"):
-		if os.path.splitext(FileName)[1] == ".py":
-			__import__(os.path.splitext(FileName)[0])
+	
+	PluginsDir = os.path.dirname(sys.argv[0]) + '\\Plugins'
+	sys.path.append(PluginsDir)
+
+	Modules = []
+	AllowedExt = [ i[0] for i in imp.get_suffixes() ]
+	for ModuleName, Ext in [ os.path.splitext(f) for f in os.listdir(PluginsDir) ]: 
+		if Ext in AllowedExt and not ModuleName in Modules:
+			try:
+				Modules.append(ModuleName)
+				__import__(ModuleName)
+
+			except ImportError, e:
+				traceback.print_exc()
