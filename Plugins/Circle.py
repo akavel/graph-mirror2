718fd60c61f227eef1498786e61d16317e614ac3
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2007-06-29 21:23:37 +0000

    Minor changes.

--- Plugins/Circle.py
+++ Plugins/Circle.py
@@ -1,37 +1,32 @@
-# File: Circle.py
-import tkSimpleDialog
 import Graph
 import GraphUtil
-from Tkinter import *
+import wx
 
-class MyDialog(tkSimpleDialog.Dialog):
-	def body(self, master):
-		Label(master, text="Radius:").grid(row=0)
-		Label(master, text="Center:").grid(row=1)
-		Label(master, text="X:").grid(row=1, column=1)
-		Label(master, text="Y:").grid(row=1, column=3)
-
-		self.e1 = Entry(master, width=10)
-		self.e2 = Entry(master, width=10)
-		self.e3 = Entry(master, width=10)
-
-		self.e1.grid(row=0, column=2)
-		self.e2.grid(row=1, column=2)
-		self.e3.grid(row=1, column=4)
-
-		self.resizable(height=FALSE, width=FALSE)
-		self.geometry("+%d+%d" % (500, 300))
-
-		return self.e1 # initial focus
-
-	def validate(self):
-		#self.result = float(self.e1.get()), float(self.e2.get()), float(self.e3.get())
-		return 1
-
-	def apply(self):
-		Graph.CreateParametricFunction(self.e2.get() + "+" + self.e1.get() + "*cos t", self.e3.get() + "+" + self.e1.get() + "*sin t")
 
 def execute_action():
-	d = MyDialog(None, "Create circle")
+    class CircleDialog(wx.Dialog):
+        def __init__(self, parent, title):
+            wx.Dialog.__init__(self, parent, wx.ID_ANY, title, size=(200,100))
+            wx.Button(self, 10, "OK")
+            wx.Button(self, 11, "Cancel")
+            wx.EVT_BUTTON(self, 10, self.OnOk)
+            wx.EVT_BUTTON(self, 11, self.OnCancel)
 
-action = GraphUtil.Action("Create circle...", execute_action, icon="Graph.bmp", hint="Create circle from center and radius", shortcut="Ctrl+Alt+C")
+        def OnOk(self, event):
+            print "OK"
+            d = ScrolledMessageDialog(GraphUtil.root, "Hello world", "Test")
+            d.ShowModal()
+            pass
+
+        def OnCancel(self, event):
+            Close()
+
+    from wx.lib.dialogs import ScrolledMessageDialog
+#    d = ScrolledMessageDialog(GraphUtil.root, "Hello world", "Test")
+#    d = wx.MessageDialog(GraphUtil.root, "Hello world")
+    d = CircleDialog(GraphUtil.root, 'Small editor')
+    d.ShowModal()
+    print "Hello world"
+
+
+action = GraphUtil.Action("Create circle...", execute_action, icon="Graph.bmp", hint="Create circle from center and radius", shortcut="Ctrl+Shift+C")
--- Plugins/GraphUtil.py
+++ Plugins/GraphUtil.py
@@ -3,38 +3,44 @@ import os
 import sys
 import imp
 import traceback
-from Tkinter import *
 import Graph
+import wx
 
 def InitPlugins():
-	print "Loading plugins..."
-	sys.stdin = None
+    print "Loading plugins..."
+    sys.stdin = None
 
-	global root
-	root = Tk()
-	root.withdraw()
+    global root
+    global app
 
-	PluginsDir = os.path.dirname(sys.argv[0]) + '\\Plugins'
-	sys.path.append(PluginsDir)
+    app = wx.PySimpleApp()
+#    d = wx.MessageDialog(None, "Hello world")
+    root = wx.Window_FromHWND(None, Graph.handle)
+#    root.Reparent(root)
+#    root.Reparent(d)
+#    root.Reparent(None)
 
-	Modules = []
-	AllowedExt = [ i[0] for i in imp.get_suffixes() ]
-	for ModuleName, Ext in [ os.path.splitext(f) for f in os.listdir(PluginsDir) ]: 
-		if Ext in AllowedExt and not ModuleName in Modules:
-			try:
-				Modules.append(ModuleName)
-				__import__(ModuleName)
+    PluginsDir = os.path.dirname(sys.argv[0]) + '\\Plugins'
+    sys.path.append(PluginsDir)
 
-			except ImportError, e:
-				traceback.print_exc()
+    Modules = []
+    AllowedExt = [ i[0] for i in imp.get_suffixes() ]
+    for ModuleName, Ext in [ os.path.splitext(f) for f in os.listdir(PluginsDir) ]:
+        if Ext in AllowedExt and not ModuleName in Modules:
+            try:
+                Modules.append(ModuleName)
+                __import__(ModuleName)
+
+            except ImportError, e:
+                traceback.print_exc()
 
 
 class Action(object):
-		def __init__(self, caption, event, **keywords): 
-			object.__setattr__(self, "id", Graph.CreateAction())
-			Graph.SetActionAttr(self.id, caption=caption, event=event, **keywords) 
+    def __init__(self, caption, event, **keywords):
+        object.__setattr__(self, "id", Graph.CreateAction())
+        Graph.SetActionAttr(self.id, caption=caption, event=event, **keywords)
 
-		def __getattr__(self, name): 
-			return Graph.GetActionAttr(self.id)[name]
-		def __setattr__(self, name, value): 
-			Graph.SetActionAttr(self.id, **{name:value})
+    def __getattr__(self, name):
+        return Graph.GetActionAttr(self.id)[name]
+    def __setattr__(self, name, value):
+        Graph.SetActionAttr(self.id, **{name:value})
