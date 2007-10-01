61521a6c450cef869597a3f20e55c01496a7444b
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2007-10-01 19:41:43 +0000

    Beta version is now available from web site.

--- Trunk/WebSite/graph/Beta.php
+++ Trunk/WebSite/graph/Beta.php
@@ -1,23 +1,13 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-
-  
-
-
-  
   <title>Graph - Download beta</title>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-
-
-  
   <link rel="stylesheet" href="Graph.css" type="text/css">
 </head>
 
 
 <body>
-
-
 <div id="main">
 <h1 align="center"><font color="#0000ff" size="+7"><b>Beta test</b></font></h1>
 
