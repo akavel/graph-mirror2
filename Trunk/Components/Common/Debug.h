d5895937a6cff5847d31f7a940bf19d8d37106fd
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2007-12-01 16:38:52 +0000

    Simplified debug system.

--- Trunk/Components/Common/Debug.h
+++ Trunk/Components/Common/Debug.h
@@ -6,7 +6,6 @@ struct TNameValue
 {
   const char *Name;
   unsigned Value;
-  TNameValue(const char *AName, unsigned AValue) : Name(AName), Value(AValue) {}
 };
 
 extern const TNameValue OleUIResultList[];
@@ -15,8 +14,8 @@ extern const TNameValue HResultList[];
 //Defines and functions used for logging OLE calls
 #define LOG_FUNCTION_CALL(x) DebugLogFunctionCall(#x, HResultList, (x))
 #define LOG_OLEUI_CALL(x) DebugLogFunctionCall(#x, OleUIResultList, (x))
-#define NAME_VALUE_ENTRY(x) TNameValue(#x, x)
-#define NAME_VALUE_END TNameValue(NULL, 0)
+#define NAME_VALUE_ENTRY(x) {#x, x}
+#define NAME_VALUE_END {NULL, 0}
 
 HRESULT DebugLogFunctionCall(const char *Name, const TNameValue List[], HRESULT Result);
 AnsiString ValueToStr(const TNameValue List[], unsigned Value);
