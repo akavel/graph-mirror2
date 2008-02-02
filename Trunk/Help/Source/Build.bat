725b6ef2a69fe2ae17273f938f08df91f3344729
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2008-02-02 00:04:05 +0000

    Hyperbolic functions documented.

--- Trunk/Help/Source/Build.bat
+++ Trunk/Help/Source/Build.bat
@@ -4,13 +4,13 @@ XCopy Images\*.* Temp\Images /I /Q /Y
 
 echo.
 echo Create PDF file...
-D:\Projects\Diverse\TortoiseSvn\Tools\xsltproc.exe --nonet --xinclude --output Temp\Graph.fo ..\xsl\pdfdoc.xsl Graph.xml
-call D:\Projects\Diverse\TortoiseSvn\Tools\fop\fop.bat -q -fo Temp\Graph.fo -pdf Graph.pdf
+..\..\Tools\xsltproc.exe --nonet --xinclude --output Temp\Graph.fo ..\xsl\pdfdoc.xsl Graph.xml
+call ..\..\Tools\fop\fop.bat -q -fo Temp\Graph.fo -pdf Graph.pdf
 
 echo.
 echo Create CHM file...
-D:\Projects\Diverse\TortoiseSvn\Tools\xsltproc.exe --nonet --xinclude --stringparam html.stylesheet styles_chm.css --stringparam use.id.as.filename 1 --output Temp\Graph ..\xsl\htmlhelp.xsl Graph.xml
-D:\Projects\Diverse\TortoiseSvn\Tools\hhc.exe Temp\htmlhelp.hhp > NUL
+..\..\Tools\xsltproc.exe --nonet --xinclude --stringparam html.stylesheet styles_chm.css --stringparam use.id.as.filename 1 --output Temp\Graph ..\xsl\htmlhelp.xsl Graph.xml
+..\..\Tools\hhc.exe Temp\htmlhelp.hhp
 
 echo.
 pause
