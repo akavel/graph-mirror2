9faef0ff6fde10fb3a818b013d3d437b2d7412e5
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2008-02-14 22:36:31 +0000

    Updated

--- Trunk/Help/Output/Build.bat
+++ Trunk/Help/Output/Build.bat
@@ -7,13 +7,13 @@ XCopy ..\Source\*.css Temp /Q /Y
 cd ..\Source
 echo.
 echo Create PDF file...
-..\..\Tools\xsltproc.exe --nonet --xinclude --output ..\Output\Temp\Graph.fo ..\xsl\pdfdoc.xsl Graph.xml
-call ..\..\Tools\fop\fop.bat -q -fo ..\Output\Temp\Graph.fo -pdf ..\Output\Graph.pdf
+..\..\Tools\xsltproc.exe --nonet --xinclude --output ..\\Temp\Graph.fo xsl\pdfdoc.xsl Graph.xml
+call ..\..\Tools\fop\fop.bat -q -fo ..\Temp\Graph.fo -pdf ..\Graph-English.pdf
 
 echo.
 echo Create CHM file...
-..\..\Tools\xsltproc.exe --nonet --xinclude --output ..\Output\Temp\Graph ..\xsl\htmlhelp.xsl Graph.xml
-..\..\Tools\hhc.exe ..\Output\Temp\htmlhelp.hhp
+..\..\Tools\xsltproc.exe --nonet --xinclude --output ..\Temp\Graph ..\xsl\htmlhelp.xsl Graph.xml
+..\..\Tools\hhc.exe ..t\Temp\htmlhelp.hhp
 
 echo.
 pause
