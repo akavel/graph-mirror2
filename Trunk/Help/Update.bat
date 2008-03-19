fc842ff9f685e4415db094d500602f300b72bc74
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2008-03-19 22:06:17 +0000

    Hurray. The help file has now been converted. A lot of adjustments are still needed.

--- Trunk/Help/Update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set hcw="%ProgramFiles%\Borland\CBuilder6\Help\Tools\hcw.exe"
-%hcw% /C /E English\Graph.hpj
-%hcw% /C /E Danish\Graph.hpj
-%hcw% /C /E German\Graph.hpj
-%hcw% /C /E French\Graph.hpj
-%hcw% /C /E Slovenian\Graph.hpj
