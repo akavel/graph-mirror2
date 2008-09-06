f36601f854e531b23a1736a52bc088fd6668c1e1
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2008-09-06 00:20:20 +0000

    All the function titles have been simplified.

--- Trunk/Help/po/Create.py
+++ Trunk/Help/po/Create.py
@@ -5,33 +5,32 @@ import codecs
 
 SourcePath = "../Source/"
 OutFile = "..\\po\\GraphHelp.pot"
-Language = "Danish"
+Languages = ["Danish", "Croatian", "Spanish"]
 LocalePath = "d:\\Projects\\Graph\\Locale\\"
-#LocalePath = "d:\\"
 
 os.chdir(SourcePath)
 Files = glob.glob("*.xml")
 os.system("..\\..\\Tools\\xml2po\\xml2po.py -k -o " + OutFile + " " + " ".join(Files))
 os.chdir("..\\po")
 
-# This is a hack to trick gettext into laoding the .mo file
-Lang = gettext.translation(LocalePath + Language, languages=["test"])
-Lang.install()
+for Language in Languages:
+    # This is a hack to trick gettext into laoding the .mo file
+    Lang = gettext.translation(LocalePath + Language, languages=["test"])
+    Lang.install()
 
-InFile = codecs.open("GraphHelp.pot", "r", "utf-8")
-OutFile = codecs.open("GraphHelp_Danish.po", "w", "utf-8")
-
-IgnoreNext = False
-for Line in InFile:
-    if not IgnoreNext:
-        OutFile.write(Line)
+    InFile = codecs.open("GraphHelp.pot", "r", "utf-8")
+    OutFile = codecs.open("GraphHelp_" + Language + ".po", "w", "utf-8")
     IgnoreNext = False
-    if Line[:5] == "msgid":
-        Str = Line[7:-3]
-        if Str:
-            Str2 = Lang.ugettext(Str)
-            if Str2 == Str:
-                Str2 = Lang.ugettext(Str + ":")[:-1]
-            if Str2 != Str:
-                OutFile.write("msgstr \"%s\"\n" % (Str2,))
-                IgnoreNext = True
+    for Line in InFile:
+        if not IgnoreNext:
+            OutFile.write(Line)
+        IgnoreNext = False
+        if Line[:5] == "msgid":
+            Str = Line[7:-3]
+            if Str:
+                Str2 = Lang.ugettext(Str)
+                if Str2 == Str:
+                    Str2 = Lang.ugettext(Str + ":")[:-1]
+                if Str2 != Str:
+                    OutFile.write("msgstr \"%s\"\n" % (Str2,))
+                    IgnoreNext = True
