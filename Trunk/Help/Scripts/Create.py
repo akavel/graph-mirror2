68a35133ed2d5f54984040c22b43c08cf49da0a0
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2008-10-31 16:47:57 +0000

    Preserve entities. Added tags for text that should not be translated.

--- Trunk/Help/Scripts/Create.py
+++ Trunk/Help/Scripts/Create.py
@@ -17,22 +17,28 @@ os.chdir("../po")
 for Language in Languages:
     print
     print Language + ":"
-    # Merge with old translation
-    os.system("..\\..\\Tools\\msgmerge -U -v -q --backup=off GraphHelp_%s.po GraphHelp.pot" % (Language,))
+    FileName = "GraphHelp_%s.po" % (Language,)
+
+    if os.path.exists(FileName):
+        # Merge with old translation
+        os.system("..\\..\\Tools\\msgmerge -U -v -q --backup=off %s GraphHelp.pot" % (FileName,))
+        InFile = FileName
+    else:
+        InFile = "GraphHelp.pot"
 
     # This is a hack to trick gettext into laoding the .mo file
     Lang = gettext.translation(LocalePath + Language, languages=["test"])
     Lang.install()
 
-    Lines = codecs.open("GraphHelp_%s.po" % (Language,), "r", "utf-8").readlines()
-    OutFile = codecs.open("GraphHelp_%s.po" % (Language,), "w", "utf-8")
+    Lines = codecs.open(InFile, "r", "utf-8").readlines()
+    OutFile = codecs.open(FileName, "w", "utf-8")
     IgnoreNext = False
     for Line in Lines:
         if not IgnoreNext:
             OutFile.write(Line)
         IgnoreNext = False
         if Line[:5] == "msgid":
-            Str = Line[7:-3]
+            Str = Line[7:-2]
             if Str:
                 Str2 = Lang.ugettext(Str)
                 if Str2 == Str:
