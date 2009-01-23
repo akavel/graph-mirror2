17891ac226b0b8b7b461c9b55e47fded748a83ac
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2009-01-23 20:17:27 +0000

    Added line break.

--- WebSite/graph/Forum.php
+++ WebSite/graph/Forum.php
@@ -7,36 +7,36 @@
 </head>
 
 <body>
-<div id="main"> 
-      <h1 align="center"><font size="+7"><b><font size="+7" color="#0000ff"> 
+<div id="main">
+      <h1 align="center"><font size="+7"><b><font size="+7" color="#0000ff">
         </font><font color="#0000ff">Support forum</font></b></font></h1>
-      <p><font size="+1">There is a mailing list available for discussions and 
-        support for Graph. A forum and newsgroup is also available as alternative 
+      <p><font size="+1">There is a mailing list available for discussions and
+        support for Graph. A forum and newsgroup is also available as alternative
         interfaces to the list. It is the same messages with three different interfaces.</font></p>
       <p><font size="+1"><b>Mailing list</b></font><br>
-        <font size="+1">When subscriped to the Graph mailing list you receive 
-        and email each time a message is posted to the list. Subscription may 
-        be done from the <a href="https://lists.sourceforge.net/lists/listinfo/graph-support" target="_top">info 
+        <font size="+1">When subscriped to the Graph mailing list you receive
+        and email each time a message is posted to the list. Subscription may
+        be done from the <a href="https://lists.sourceforge.net/lists/listinfo/graph-support" target="_top">info
         page</a>.</font></p>
-      <p><font size="+1">To post a message to the list, send an email to <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#114;&#97;&#112;&#104;&#45;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#64;&#108;&#105;&#115;&#116;&#115;&#46;&#115;&#111;&#117;&#114;&#99;&#101;&#102;&#111;&#114;&#103;&#101;&#46;&#110;&#101;&#116;">&#103;&#114;&#97;&#112;&#104;&#45;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#64;&#108;&#105;&#115;&#116;&#115;&#46;&#115;&#111;&#117;&#114;&#99;&#101;&#102;&#111;&#114;&#103;&#101;&#46;&#110;&#101;&#116;</a></font></p>
-      <p><font size="+1">You can find a collection of all prior messages in the 
+      <p><font size="+1">To post a message to the list, send an email to <br><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#114;&#97;&#112;&#104;&#45;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#64;&#108;&#105;&#115;&#116;&#115;&#46;&#115;&#111;&#117;&#114;&#99;&#101;&#102;&#111;&#114;&#103;&#101;&#46;&#110;&#101;&#116;">&#103;&#114;&#97;&#112;&#104;&#45;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#64;&#108;&#105;&#115;&#116;&#115;&#46;&#115;&#111;&#117;&#114;&#99;&#101;&#102;&#111;&#114;&#103;&#101;&#46;&#110;&#101;&#116;</a></font></p>
+      <p><font size="+1">You can find a collection of all prior messages in the
         <a href="http://sourceforge.net/mailarchive/forum.php?forum=graph-support" target="_top">Archives</a>.</font></p>
-      <p><font size="+1"><b>Forum</b></font><br><font size="+1">Instead of using the 
-        mailing list directly you can read it and write to the list as a forum 
+      <p><font size="+1"><b>Forum</b></font><br><font size="+1">Instead of using the
+        mailing list directly you can read it and write to the list as a forum
         at Gmane:</font><br>
         <font size="+1"><a href="http://news.gmane.org/gmane.comp.mathematics.graph" target="_top">http://news.gmane.org/gmane.comp.mathematics.graph</a></font></p>
-      <p><font size="+1">To post messages you need a working email address. The 
-        first time you post a message you will receive an email you need to reply 
-        to, to confirm that you are a real person. To prevent spam your email 
-        address will be shown a little obfuscated in the forum message. More information 
+      <p><font size="+1">To post messages you need a working email address. The
+        first time you post a message you will receive an email you need to reply
+        to, to confirm that you are a real person. To prevent spam your email
+        address will be shown a little obfuscated in the forum message. More information
         may be found in the Gmane <a href="http://gmane.org/faq.php" target="_top">FAQ</a>.</font></p>
       <p><font size="+1"><b>Newsgroup</b><br>
-        </font><font size="+1">Gmane also supply a newsgroup interface to the 
-        list, which enables you to use your favorite newsgroup client to interface 
+        </font><font size="+1">Gmane also supply a newsgroup interface to the
+        list, which enables you to use your favorite newsgroup client to interface
         to the list:</font><br>
         <font size="+1"><a href="nntp://news.gmane.org/gmane.comp.mathematics.graph">nntp://news.gmane.org/gmane.comp.mathematics.graph</a></font></p>
-      <p><font size="+1">The above link should subscripe your default newsgroup 
-        client to the graph newsgroup. If not you have to check the documentation 
+      <p><font size="+1">The above link should subscripe your default newsgroup
+        client to the graph newsgroup. If not you have to check the documentation
         following your client for how to subscripe to it.</font></p>
 </div>
 
