39a1cc9611b9d8ab83815bcde31967a9e370560e
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2009-08-03 09:02:57 +0000

    Added new web forum.

--- WebSite/graph/index.php
+++ WebSite/graph/index.php
@@ -10,43 +10,43 @@
 </head>
 <body>
 
-<div id="main"> 
-  <h1  align="center"><b><font size="+7" color="#0000ff"> 
+<div id="main">
+  <h1  align="center"><b><font size="+7" color="#0000ff">
     Graph 4.3</font></b></h1>
   <p><img height="424" alt="Main window" src="DialogMain.png" width="595" ></p>
   <table border="0" align="center" cellspacing="10">
-    <tr> 
-      <td width="550"> 
-        <div align="left"> 
-          <p><font size="+1">Graph is an open source application used to draw 
-            mathematical graphs in a coordinate system. Anyone who wants to draw 
-            graphs of functions will find this program useful. The program makes 
-            it very easy to visualize a function and paste it into another program. 
+    <tr>
+      <td width="550">
+        <div align="left">
+          <p><font size="+1">Graph is an open source application used to draw
+            mathematical graphs in a coordinate system. Anyone who wants to draw
+            graphs of functions will find this program useful. The program makes
+            it very easy to visualize a function and paste it into another program.
             It is also possible to do some mathematical calculations on the functions.</font></p>
         </div>
       </td>
       <td valign="top">
 <table border="1" cellpadding="0" cellspacing="0" style="BORDER-COLLAPSE: collapse" width="170" id="AutoNumber4">
-          <tr> 
-            <td align="center" bgcolor="#000000"> 
-              <p><font face="Arial, Arial, Helvetica"> <b><font color="#ffff00" size="2">Graph 
-                Update </font></b></font><font face="Arial, Arial, Helvetica"><b><font color="#ffff00" size="2">Mailing 
+          <tr>
+            <td align="center" bgcolor="#000000">
+              <p><font face="Arial, Arial, Helvetica"> <b><font color="#ffff00" size="2">Graph
+                Update </font></b></font><font face="Arial, Arial, Helvetica"><b><font color="#ffff00" size="2">Mailing
                 List</font></b> </font></p>
             </td>
           </tr>
-          <tr> 
-            <td align="center" bgcolor="#ffff99"> 
+          <tr>
+            <td align="center" bgcolor="#ffff99">
               <form method="post" action="http://www.padowan.dk/graph/updatelist.php">
-                <p align="center" style="MARGIN-TOP: 0px; MARGIN-BOTTOM: 2px"> <font size="2" face="Arial, Arial, Helvetica">Join 
+                <p align="center" style="MARGIN-TOP: 0px; MARGIN-BOTTOM: 2px"> <font size="2" face="Arial, Arial, Helvetica">Join
                   the list to be notified when a new version of Graph is available.</font></p>
-                <p align="center" style="MARGIN-TOP: 0px; MARGIN-BOTTOM: 2px"> <font face="Arial, Arial, Helvetica" size="1">Enter 
+                <p align="center" style="MARGIN-TOP: 0px; MARGIN-BOTTOM: 2px"> <font face="Arial, Arial, Helvetica" size="1">Enter
                   your email address:</font><font face="Arial, Arial, Helvetica"><br>
                   <input name="email" size="23" >
                   </font></p>
-                <p style="MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px" align="center" 
-           > <font face="Arial, Arial, Helvetica"> 
+                <p style="MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px" align="center"
+           > <font face="Arial, Arial, Helvetica">
                   <input type="image" alt="Click here to join the Graph update mailing list" src="subscribe.png" >
-                  </font> 
+                  </font>
               </form>
             </td>
           </tr>
@@ -54,9 +54,9 @@
       </td>
     </tr>
   </table>
-  <p align="center"><b><font size="+1">Copyright © 2007 by Ivan Johansen<br>
-  Last updated: 
-    August 26, 2007
+  <p align="center"><b><font size="+1">Copyright © 2009 by Ivan Johansen<br>
+  Last updated:
+    August 2, 2009
     </font></b></p>
 </div>
 
