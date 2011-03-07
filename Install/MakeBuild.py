89ed5ee37d81b92f8b3a219eb80d81b8279a5d04
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2011-03-07 20:11:25 +0000

    Scripts upgraded to Python 3

--- Install/MakeBuild.py
+++ Install/MakeBuild.py
@@ -10,30 +10,31 @@ try:
   # Test that Graph can be started without some obscure dll files
   os.system("Graph.exe /regserver")
 
-  print "Updating Graph.iss ..."
-  File = open("Graph.iss")
+  print("Updating Graph.iss ...")
+  File = open("Graph.iss", "rb")
   Lines = File.readlines()
+  File.close()
   VersionInfo = CalcVersionInfo("Graph.exe")
   BaseFileName = "SetupGraphBeta-" + VersionInfo
   FileName = BaseFileName + ".exe"
   Count = 0
   for Line in Lines:
-    if Line[:18] == "VersionInfoVersion":
-      Lines[Count] = "VersionInfoVersion=" + VersionInfo + "\n"
-    if Line[:18] == "OutputBaseFilename":
-      Lines[Count] = "OutputBaseFilename=" + BaseFileName + '\n'
+    if Line[:18] == b"VersionInfoVersion":
+      Lines[Count] = b"VersionInfoVersion=" + VersionInfo.encode() + b"\n"
+    if Line[:18] == b"OutputBaseFilename":
+      Lines[Count] = b"OutputBaseFilename=" + BaseFileName.encode() + b'\n'
     Count += 1
 
-  File = open("Graph.iss", "w")
+  File = open("Graph.iss", "wb")
   File.writelines(Lines)
   File.close()
 
   # Compile SetupGraphBeta-4.2.0.x.exe
-  print "Compiling", FileName, "..."
+  print("Compiling", FileName, "...")
   os.system('"c:\progra~1\\Inno Setup 5\\iscc.exe" /Q Graph.iss')
 
   #Creating GraphBeta.inf
-  print "Writing GraphBeta.inf ..."
+  print("Writing GraphBeta.inf ...")
   File = open("GraphBeta.inf", "w")
   File.write("[Graph]\n")
   File.write("Major = " + VersionInfo[0] + "\n")
@@ -42,21 +43,22 @@ try:
   File.write("Build = " + VersionInfo[6:] + "\n")
   File.write("Date = " + datetime.date.today().strftime("%d-%m-%Y\n"))
   File.write("DownloadFile = http://www.padowan.dk/bin/" + FileName + '\n')
+  File.write("DownloadPage = http://www.padowan.dk/graph/Beta.php\n")
 
   # Upload SetupGraphBeta.exe to the server
   ftp = FTP('ftp.padowan.dk')   # connect to host, default port
   ftp.login('padowan.dk', getpass())
   ftp.cwd('bin')
-  print "Uploading", FileName, "..."
-  File=open(FileName, 'rb')
+  print("Uploading", FileName, "...")
+  File = open(FileName, 'rb')
   ftp.storbinary('STOR ' + FileName, File)
-  
-  print "Uploading GraphBeta.inf ..."
+
+  print("Uploading GraphBeta.inf ...")
   ftp.cwd('../graph')
-  File=open("GraphBeta.inf", 'rb')
+  File = open("GraphBeta.inf", 'rb')
   ftp.storbinary('STOR GraphBeta.inf', File)
   ftp.quit()
-  print "Upload complete!"
+  print("Upload complete!")
 except:
   traceback.print_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
   sys.stdin.readline()
