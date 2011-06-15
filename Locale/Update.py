52700ebc8c84ffaeaa8646bd20c3876f9c7f2b4b
IvanJohansen <IvanJohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2011-06-15 19:50:04 +0000

    Updated script.

--- Locale/Update.py
+++ Locale/Update.py
@@ -7,39 +7,39 @@ InFile = open("Temp.po")
 Lines = InFile.readlines()
 List = {}
 for I in range(len(Lines) - 1, 0, -1):
-	Line = Lines[I][:-1]
- 	if Line[:5] == "msgid":
-		MsgId = Line[7:-1]
-	if Line[:2] == "#.":
-		List[Line[3:]] = MsgId
+    Line = Lines[I][:-1]
+    if Line[:5] == "msgid":
+        MsgId = Line[7:-1]
+    if Line[:2] == "#.":
+        List[Line[3:]] = MsgId
 
 OutFile = open("Graph.pot", "w")
 RcFile = open("../Source/Source/Resource.rc")
 RcLines = RcFile.readlines()
 
 for Line in Lines:
-	if Line == 'msgstr "Project-Id-Version: PACKAGE VERSION\\n"\n':
-		OutFile.write('msgstr "Project-Id-Version: Graph\\n"\n')
-	elif Line[:21] == "#. Programmer's name:":
-		ID = Line[22:-1]
-		for RcLine in RcLines:
-			if RcLine.find(ID) != -1:
-				Pos = RcLine.find("//")
-				if Pos != -1:
-					OutFile.write("#. " + RcLine[Pos+2:])
-	elif Line[:2] == "#.":
-		Words = Line[3:].split("..")
-		NewLine = "#. "
-		Key = ""
-		for Word in Words:
-			Key += Word + ".."
-			if List.has_key(Key + "Caption"):
-				NewLine += Word + '("' + List[Key + "Caption"] + '")..'
-			else:
-				NewLine += Word + ".."
-		OutFile.write(NewLine[:-2])
-	elif Line[:2] != "#:":
-		OutFile.write(Line)
+    if Line == 'msgstr "Project-Id-Version: PACKAGE VERSION\\n"\n':
+        OutFile.write('msgstr "Project-Id-Version: Graph\\n"\n')
+    elif Line[:21] == "#. Programmer's name:":
+        ID = Line[22:-1]
+        for RcLine in RcLines:
+            if RcLine.find(ID) != -1:
+                Pos = RcLine.find("//")
+                if Pos != -1:
+                    OutFile.write("#. " + RcLine[Pos+2:])
+    elif Line[:2] == "#.":
+        Words = Line[3:].split("..")
+        NewLine = "#. "
+        Key = ""
+        for Word in Words:
+            Key += Word + ".."
+            if (Key + "Caption") in List:
+                NewLine += Word + '("' + List[Key + "Caption"] + '")..'
+            else:
+                NewLine += Word + ".."
+        OutFile.write(NewLine[:-2])
+    elif Line[:2] != "#:":
+        OutFile.write(Line)
 
 InFile.close()
 os.remove("default.po")
