4279cee682d93558b7cacbeb37255746ce16eca4
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2012-10-06 23:43:18 +0000

    Fixed problem with copy-paste of data with other decimal separator than dot.

--- Components/Common/HandleCSV.h
+++ Components/Common/HandleCSV.h
@@ -8,8 +8,13 @@
 //---------------------------------------------------------------------------
 typedef std::vector<std::string> TCsvRow;
 typedef std::vector<TCsvRow> TCsvGrid;
-char GetSeparator(const std::string &Str2);
-bool ImportCsv(std::istream &Stream, TCsvGrid &CsvGrid, char Separator=0);
-bool ImportCsv(const std::string &Str, TCsvGrid &CsvGrid, char Separator=0);
-bool ImportCsvFromFile(const std::wstring &FileName, TCsvGrid &CsvGrid, char Separator=0);
+extern const std::string DefaultSeparators;
+char GetSeparator(const std::string &Str2, const std::string &Separators = DefaultSeparators);
+bool ImportCsv(std::istream &Stream, TCsvGrid &CsvGrid, const std::string &Separator=DefaultSeparators);
+bool ImportCsv(const std::string &Str, TCsvGrid &CsvGrid, const std::string &Separator=DefaultSeparators);
+bool ImportCsvFromFile(const std::wstring &FileName, TCsvGrid &CsvGrid, const std::string &Separator=DefaultSeparators);
+
+bool ImportCsv(std::istream &Stream, TCsvGrid &CsvGrid, char Separator);
+bool ImportCsv(const std::string &Str, TCsvGrid &CsvGrid, char Separator);
+bool ImportCsvFromFile(const std::wstring &FileName, TCsvGrid &CsvGrid, char Separator);
 #endif
