208c043a04580bc7fb08a3036defbb27c6b6f080
ivanjohansen <ivanjohansen@25502e89-caf0-4f7c-9905-56120a8fff7e>
2013-07-09 13:55:56 +0000

    Improved console script.

--- Trunk/PyVcl/Scripts/Console.py
+++ Trunk/PyVcl/Scripts/Console.py
@@ -1,5 +1,6 @@
 from PyVcl import vcl
-
+import sys
+   
 class ConsoleForm:
   def __init__(self):
     self.LastIndex = 0
@@ -7,16 +8,39 @@ class ConsoleForm:
     self.TextCache = [""]
     self.IndentLevel = 0
     
-    self.Form = vcl.TForm(None) 
+    self.Form = vcl.TForm(None, OnClose=self.Close, Width=600) 
     self.Form.Caption = "Python interpreter"
-    self.RichEdit = vcl.TRichEdit(None, Parent=self.Form, Align="alClient", OnKeyDown=self.RichEditKeyDown)
+    self.RichEdit = vcl.TRichEdit(None, Parent=self.Form, Align="alClient", ScrollBars="ssVertical", WantTabs=True,
+      OnKeyDown=self.KeyDown, OnKeyPress=self.KeyPress)      
     self.RichEdit.Font.Name = "Courier New"
     self.RichEdit.Font.Size = 10
+    self.OldWindowProc = self.RichEdit.WindowProc
+#    self.RichEdit.WindowProc=self.WindowProc     
     
     self.PopupMenu = vcl.TPopupMenu(None)
     self.PopupMenu.Items.Add(vcl.TMenuItem(self.PopupMenu, Caption="Clear", _owned=False, OnClick=self.Clear))
     self.Form.PopupMenu = self.PopupMenu
-    self.WritePrompt()
+
+    self.Form.Canvas.Font.Assign(self.RichEdit.Font)
+    CharWidth = (self.Form.Canvas.TextWidth("A") * 15) // 22
+    self.RichEdit.Paragraph.TabCount = 22
+    x = CharWidth * 4
+    for I in range(0, 22): 
+      x += CharWidth * 2
+      self.RichEdit.Paragraph.Tab[I] = x
+    
+    self.WritePrompt()   
+    self.stdout = sys.stdout
+    sys.stdout = self
+  
+  def write(self, Str):
+    self.WriteText(Str, 0)
+    
+  def flush(self):
+    pass
+  
+  def Close(self, Sender, Action):
+    sys.stdout = self.stdout
     
   def Clear(self, Sender):
     self.RichEdit.Clear()
@@ -24,14 +48,63 @@ class ConsoleForm:
     self.PromptIndex = 0
     self.WritePrompt()
     
-  def RichEditKeyDown(self, Sender, Key, Shift):
-    if Key.Value == 0x0D: #VK_RETURN
-      if Shift == "":
-        self.HandleNewLine()
-        Key = 0
-        
-  def HandleNewLine(self):
-    Str = self.RichEdit.Text[self.LastIndex:].rstrip()
+  def WriteText(self, Str, Color):
+    OldSelStart = self.RichEdit.SelStart
+    self.RichEdit.SelStart = self.PromptIndex
+    self.RichEdit.SelAttributes.Color = Color
+    self.RichEdit.SelText = Str
+    IndexChange = self.RichEdit.SelStart - self.PromptIndex
+    self.LastIndex += IndexChange
+    self.PromptIndex = self.RichEdit.SelStart
+    self.RichEdit.SelStart = OldSelStart + IndexChange
+    self.RichEdit.SelLength = 0
+  
+  def KeyDown(self, Sender, Key, Shift):
+    if (Shift == "ssCtrl" and Key.Value == ord("V")) or (Shift == "ssShift" and Key.Value == 0x2D): #0x2D=VK_INSERT
+      pass
+    elif Shift == "ssCtrl" and Key.Value == ord("C"):
+      if self.RichEdit.SelLength == 0:
+        self.KeyboardInterrupt()
+    elif Key.Value == 0x08: #Backspace  
+      if self.RichEdit.SelStart <= self.LastIndex:
+        Key.Value = 0
+    elif Key.Value == 0x2E: #Delete
+      if self.RichEdit.SelStart < self.LastIndex:
+        Key.Value = 0      
+    elif Key.Value == 0x0D: #VK_RETURN
+      self.HandleNewLine()
+      Key.Value = 0
+    elif Shift == "" and Key.Value == 0x1B: #VK_ESCAPE
+      self.SetUserString("");
+    elif Shift == "" and Key.Value == 0x26: #VK_UP
+      if self.RichEdit.ActiveLineNo == self.RichEdit.Lines.Count - 1:
+        if self.CacheIndex > 0:
+          self.RichEdit.SelStart = self.LastIndex
+          self.RichEdit.SelLength = 0x7FFFFFFF
+          if self.CacheIndex == len(self.TextCache) - 1:
+            self.TextCache[-1] = self.GetUserString()
+          self.CacheIndex -= 1 
+          self.SetUserString(self.TextCache[self.CacheIndex])
+        Key.Value = 0
+    elif Shift == "" and Key.Value == 0x28: #VK_DOWN
+      pass
+    
+  def KeyPress(self, Sender, Key):
+    if self.RichEdit.SelStart < self.LastIndex:
+      Key.Value = "\0"
+  
+  def KeyboardInterrupt(self):
+    self.RichEdit.SelStart = 0x7FFFFFFF
+    self.RichEdit.SelText = "\r"
+    self.PromptIndex = self.RichEdit.SelStart
+    self.WriteText("KeyboardInterrupt\r\n", 0xFF)
+    self.WritePrompt()
+    self.Command = ""
+    self.IndentLevel = 0
+    self.CacheIndex = len(self.TextCache) - 1
+  
+  def HandleNewLine(self):  
+    Str = self.GetUserString()
     self.RichEdit.SelStart = 0x7FFFFFFF
     self.RichEdit.SelText = "\r";
     self.PromptIndex = self.RichEdit.SelStart
@@ -44,31 +117,47 @@ class ConsoleForm:
     
     try:
       import code
-      Code = code.compile_command(Str, "<console>")
+      Code = code.compile_command(self.Command, "<console>")
       if Code:
-        exec(Code)
+        exec(Code, globals(), globals())
         self.WritePrompt()
         self.Command = ""
         self.IndentLevel = 0
       else:  
-        self.WritePromtp("... ")
+        self.WritePrompt("... ")
         self.Command += "\n"
         if Str != "" and Str[-1] == ":":
           self.IndentLevel += 1
     except:
       import traceback
-      traceback.print_exc()
+      TraceBack = traceback.format_exception(*sys.exc_info())
+      self.WriteText(TraceBack[0], 0xFF)
+      for Str in TraceBack[2:]: self.WriteText(Str, 0xFF)
       self.WritePrompt()
       self.Command = ""
       self.IndentLevel = 0
     self.RichEdit.SelText = "\t" * self.IndentLevel  
     
   def WritePrompt(self, Str=">>> "):
+    self.RichEdit.SelStart = 0x7FFFFFFF
+    self.PromptIndex = self.RichEdit.SelStart
+    self.RichEdit.SelAttributes.Color = 0
     self.RichEdit.SelText = Str
     self.LastIndex = self.RichEdit.SelStart
   
+  def SetUserString(self, Str):
+    self.RichEdit.SelStart = self.LastIndex
+    self.RichEdit.SelLength = 0x7FFFFFFF
+    self.RichEdit.SelText = Str
+  
+  def GetUserString(self):
+    return self.RichEdit.Text.rstrip().replace('\r', "")[self.LastIndex:]
+  
   def ShowModal(self):
     return self.Form.ShowModal()
+    
+  def WindowProc(self, Message):
+    self.OldWindowProc(Message)
     
 if __name__ == "__main__":
   Form = ConsoleForm()
